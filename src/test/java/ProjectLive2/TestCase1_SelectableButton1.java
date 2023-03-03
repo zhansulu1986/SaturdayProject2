a45614406aa61364c6e9b768809f7738985170ed
serin <serine.ghazaryan@gmail.com>
2023-03-03 12:56:34 -0800

    TestCase2_SelectableButton2

--- src/test/java/ProjectLive2/TestCase1_SelectableButton1.java
+++ src/test/java/ProjectLive2/TestCase1_SelectableButton1.java
@@ -7,19 +7,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class TestCase1_SelectableButton1 {
-    public static void main(String[] args) {
+public class TestCase1_SelectableButton1 extends UtilityClass{
+    public static void main(String[] args) throws InterruptedException {
         /*Validate the working of Selectable functionality
         1.Go to the url:https://demoqa.com/selectable/
         2. Click on Item 1 on the selectable list;
         After selecting Item 1, you should see Item 1 is highlighted*/
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+
 
         driver.get("https://demoqa.com/selectable/");
 
-        WebElement first=driver.findElement(By.xpath("//li[@class='mt-2 list-group-item list-group-item-action']"));
+       WebElement first=driver.findElement(By.xpath("//li[@class='mt-2 list-group-item list-group-item-action']"));
+
+
         first.click();
 
 //        WebElement selected=driver.findElement(By.xpath("//li[@class='mt-2 list-group-item active list-group-item-action']"));
---
+++ src/test/java/ProjectLive2/TestCase2_SelectableButton2.java
@@ -0,0 +1,52 @@
+package ProjectLive2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class TestCase2_SelectableButton2 extends UtilityClass {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        /*
+        Validate selecting multiple items with ctrl/cmd button
+
+        Go to the url:
+
+        https://demoqa.com/selectable/
+
+        Press ctrl/cmd button;
+
+        Click on Item 1 on the selectable list;
+
+        Click on Item 2 on the selectable list;
+
+        Click on Item 3 on the selectable list;
+
+        Release ctrl/cmd button;
+
+        After selecting Item 1, 2 and 3 with ctrl/cmd button, you should see Item 1, 2 and 3 are highlighted
+         */
+
+        driver.get("https://demoqa.com/selectable/");
+
+        WebElement first=driver.findElement(By.xpath("//li[@class='mt-2 list-group-item list-group-item-action'][1]"));
+        WebElement second=driver.findElement(By.xpath("//li[@class='mt-2 list-group-item list-group-item-action'][2]"));
+        WebElement third=driver.findElement(By.xpath("//li[@class='mt-2 list-group-item list-group-item-action'][3]"));
+        Actions actions=new Actions(driver);
+        Action cmd1=actions.keyDown(Keys.COMMAND).moveToElement(first).click().build();
+        cmd1.perform();
+        Thread.sleep(2000);
+        Action cmd2=actions.keyDown(Keys.COMMAND).moveToElement(second).click().build();
+        cmd2.perform();
+        Thread.sleep(2000);
+        Action cmd3=actions.keyDown(Keys.COMMAND).moveToElement(third).click().build();
+        cmd3.perform();
+        Action cmdRelease=actions.keyUp(Keys.COMMAND).build();
+        cmdRelease.perform();
+
+
+    }
+}
